5c40db5cfa0c6ef1dfd06e5cf417d79a531a6488
Xamacardoso <xcardoso1997@gmail.com>
2025-01-21 22:21:20 -0300

    Feat(General): Added domain model entities and its relational mapping

---
+++ src/main/java/com/xamacardoso/gamesList/entities/BelongingPK.java
@@ -0,0 +1,57 @@
+package com.xamacardoso.gamesList.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+// Represents a primary key that is composed by more than one foreign key
+@Embeddable // Annotation for representing encapsulation of more than one attribute
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK() {
+    }
+
+    public BelongingPK(GameList gameList, Game game) {
+        this.gameList = gameList;
+        this.game = game;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
