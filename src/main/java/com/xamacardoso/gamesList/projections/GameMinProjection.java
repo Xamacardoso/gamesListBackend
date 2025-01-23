3783794070a357849eb43102dc4f8c115313ab81
Xamacardoso <xcardoso1997@gmail.com>
2025-01-23 10:48:18 -0300

    Fix (SQL Query): Fixed syntax error on getting games by id and adapted projection

--- src/main/java/com/xamacardoso/gamesList/projections/GameMinProjection.java
+++ src/main/java/com/xamacardoso/gamesList/projections/GameMinProjection.java
@@ -5,7 +5,7 @@ package com.xamacardoso.gamesList.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
